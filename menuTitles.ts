5e89dcc02678e66a0857008e27a317e7a8a13362
addis-ngo <addis.ngo@gmail.com>
2026-08-05 11:11:08 -0400

    Add files via upload

---
+++ menuTitles.ts
@@ -0,0 +1,8 @@
+// List of main menu titles.
+// Titles will rotate sequentially in order (not randomly).
+// You can add more title strings to this array!
+
+export const MENU_TITLES: string[] = [
+  "ichigobankai.com",
+  "sonbaru..."
+];
